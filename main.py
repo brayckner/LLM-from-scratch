6d172665a5c465c5394af72cd6044b8f545d2a18
brayckner <brayckner@gmail.com>
2026-02-28 16:07:40 -0600

    Positional Embeddings - Create the input embeddings, which is the token embeddings plus the positional imbeddings, which are now ready to feed into the LLM.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import re
+import torch
 from tokenizer.simple_tokenizer_v1 import SimpleTokenizerV1
 from tokenizer.simple_tokenizer_v2 import SimpleTokenizerV2
 from gpt_dataset.gpt_dataset_v1 import GPTDatasetV1, create_dataloader_v1
 import tiktoken
 
+torch.manual_seed(123)
 
 def read_file(text_file: str) -> str:
     with open(f"{text_file}", "r", encoding="utf-8") as f:
@@ -26,54 +28,33 @@ def main():
     # Tokenization
     raw_text = read_file("data/the-verdict.txt")
 
-    # tokenizer = tiktoken.get_encoding("gpt2")  # Utilizing BPE tokenizer, same as GPT-2
+    max_length = 4
 
-    # enc_text = tokenizer.encode(raw_text)
-    # print(len(enc_text))
-
-    # # Tokens to input-target pairs
-    # enc_sample = enc_text[50:]
-
-    # context_size = 4
-    # x = enc_sample[:context_size]
-    # y = enc_sample[1 : context_size + 1]
-
-    # print(f"x: {x}")  # Inputs
-    # print(f"y:      {y}")  # Targets
-
-    # # Next word prediction task (using tokenIDs)
-    # for i in range(1, context_size + 1):
-    #     context = enc_sample[:i]
-    #     desired = enc_sample[i]
-    #     print(context, "----->", desired)
-
-    # # Next word prediction task (using words for visual)
-    # for i in range(1, context_size + 1):
-    #     context = enc_sample[:i]
-    #     desired = enc_sample[i]
-    #     print(tokenizer.decode(context), "----->", tokenizer.decode([desired]))
-
-    # Batch Size 1
     dataloader = create_dataloader_v1(
-        raw_text, batch_size=1, max_length=4, stride=1, shuffle=False
-    )
-    data_iter = iter(dataloader)
-
-    first_batch = next(data_iter)
-    print(first_batch)
-
-    second_batch = next(data_iter)
-    print(second_batch)
-
-    # Batch Size 8
-    dataloader = create_dataloader_v1(
-        raw_text, batch_size=8, max_length=4, stride=4, shuffle=False
+        raw_text, batch_size=8, max_length=max_length, stride=max_length, shuffle=False
     )
     data_iter = iter(dataloader)
     inputs, targets = next(data_iter)
 
-    print("Inputs:\n", inputs)
-    print("\nTargets:\n", targets)
+    print("Token IDs:\n", inputs)
+    print("\nInputs shape:\n", inputs.shape)  # 8 x 4
+
+    vocab_size = 50257
+    output_dim = 256
+
+    token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+
+    token_embeddings = token_embedding_layer(inputs)
+    print(token_embeddings.shape)  # 8 x 4 x 256
+
+    context_length = max_length
+    pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+    pos_embeddings = pos_embedding_layer(torch.arange(context_length))
+    print(pos_embeddings.shape)  # 4 x 256
+
+    # Now we can add the positional embedding to the token embeddings
+    input_embeddings = token_embeddings + pos_embeddings
+    print(input_embeddings.shape)
 
 
 if __name__ == "__main__":
